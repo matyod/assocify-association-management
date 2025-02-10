0d4b96f0288226a4bae656effd3ea76f6efd3b81
matyod <109255635+matyod@users.noreply.github.com>
2025-02-10 16:37:48 +0800

    Query and render events table

--- controllers/events/index.php
+++ controllers/events/index.php
@@ -1,6 +1,13 @@
 <?php
 
+use Core\Event;
+use Core\DateTimeCustom;
+
 $title = 'All Events | Assocify';
 $h1 = 'All Events';
+
+$event = new Event($db);
+
+$events = $event->getAllEvents();
 
 require view_path('events/index.view.php');
--- views/events/index.view.php
+++ views/events/index.view.php
@@ -1,5 +1,7 @@
 <?php
 
+use Core\DateTimeCustom;
+
 require view_path('partials/top.php');
 
 ?>
@@ -12,9 +14,48 @@ require view_path('partials/top.php');
     - Ability to register for events they are not yet signed up for.
 
     For Committee Members (Admins):
-    - Display events and detailed info on registrants (who is attending).
+    - Display events, organized by what committee, and detailed info on registrants (who is attending).
     - Access to register/remove members for specific events.
   </pre>
+
+  <table>
+    NOTE: ADMIN
+    <?php if ($events ?? false): ?>
+      <tr>
+        <th>Date</th>
+        <th>Day</th>
+        <th>Time</th>
+        <th>Event</th>
+        <th>Description</th>
+        <th>Organized by</th>
+        <th>Created at</th>
+        <th>Last updated</th>
+        <th></th>
+      </tr>
+      <?php foreach ($events as $event): ?>
+        <tr>
+          <td><?= htmlspecialchars((new DateTimeCustom($event['event_date']))->formatDate_dmY() ?? ''); ?></td>
+          <td><?= htmlspecialchars((new DateTimeCustom($event['event_date']))->formatDay_l() ?? ''); ?></td>
+          <td><?= htmlspecialchars((new DateTimeCustom($event['event_date']))->formatTime_12HrsMeridiem() ?? ''); ?></td>
+          <td>
+            <?= '<a href="/events/?id=' . ($event['event_id'] ?? '') . '">' . htmlspecialchars($event['event_name'] ?? '') . '</a>'; ?>
+          </td>
+          <td><?= htmlspecialchars($event['description'] ?? ''); ?></td>
+          <td>
+            <?= '<a href="/committees/?id=' . ($event['committee_id'] ?? '') . '">' . htmlspecialchars($event['committee_name'] ?? '') . '</a>'; ?>
+          </td>
+          <td><?= htmlspecialchars((new DateTimeCustom($event['created_at']))->formatDateTime_dMYHiS() ?? ''); ?></td>
+          <td><?= htmlspecialchars((new DateTimeCustom($event['updated_at']))->formatDateTime_dMYHiS() ?? ''); ?></td>
+          <td><a href="/events/new/participant/?id=<?= htmlspecialchars($_SESSION['user_id'] ?? ''); ?>">Register as
+              participant</a></td>
+        <tr>
+        <?php endforeach; ?>
+        <?php else: ?>
+      <tr>
+        <td>No events found.</td>
+      </tr>
+    <?php endif; ?>
+  </table>
 </div>
 
 <?php
