c253c950e48c3ac20480f199f0d4d254081c8a58
matyod <109255635+matyod@users.noreply.github.com>
2025-02-13 09:24:46 +0800

    Fix object name

--- controllers/members/index.php
+++ controllers/members/index.php
@@ -5,8 +5,8 @@ use Core\Member;
 $title = 'All Members | Assocify';
 $h1 = 'All Members';
 
-$member = new Member($db);
+$memberObj = new Member($db);
 
-$members = $member->getAllMembers();
+$members = $memberObj->getAllMembers();
 
 require view_path('members/index.view.php');
