cc91a6bcc3fb1b6b8b18ecdba474e48b1734f6dd
matyod <109255635+matyod@users.noreply.github.com>
2025-02-15 23:36:45 +0800

    Add autocomplete to username field

--- views/authentication/index.view.php
+++ views/authentication/index.view.php
@@ -22,7 +22,7 @@ require view_path('partials/header.php');
         <div>
           <label for="username" class="block text-sm/6 font-medium text-gray-900">Username</label>
           <div class="mt-2">
-            <input type="text" name="username" id="username" autocomplete="username" required
+            <input type="text" name="username" id="username" autocomplete="username" required autofocus
               class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
               value="<?= htmlspecialchars($_POST['username'] ?? ''); ?>">
           </div>
